93181fb7361069276880e9c4f345446b6cf16dda
volirra <127318805+volirra@users.noreply.github.com>
2023-10-30 17:01:37 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import Profile from 'components/Profile/Profile';
+import user from 'data/user.json';
+import Statistics from 'components/Statistics/Statistics';
+import statisticalData from 'data/statistical-data.json';
+import FriendList from 'components/FriendList/FriendList';
+import FriendList from 'data/friends.json';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import transactions from 'data/transactions.json';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList ={FriendList} />
+      <TransactionHistory items={transactions} />
+   );
+}
+
+export default App;
